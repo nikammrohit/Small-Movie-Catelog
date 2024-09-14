794e4e77592318fbe4a417e09f8f268c2e01cd5e
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-14 15:08:58 -0500

    Allows for updating the list of movies when search is clicked. Removed extra spaces in API Call. Closed functions to fix bugs

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ const API_URL = "http://www.omdbapi.com?apikey=a030ee53";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-
+  const [searchTerm, setSearchTerm] = useState("");
   //fetch movies
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL} &s= ${title}`);
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json(); //wait for response but store info in "data"
 
     setMovies(data.Search);
@@ -27,14 +27,15 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for Movies"
-          value="Superman" //static set value within movie serach bar
-          onChange={() => {}}
+          value={searchTerm} //dynamiclly set value within movie serach bar
+          onChange={(e) => setSearchTerm(e.target.value)} //allows for typing within the search bar
         />
 
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => {}} //add onClick function to make the svg a button with a function
+          onClick={() => searchMovies(searchTerm)} //add onClick function to make the svg a button with a function
+          //The function being to dynamically change the state of the window whern search button is clicked so results pop up
         />
       </div>
 
