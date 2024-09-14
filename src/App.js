ea86a285be77da49ef8ea9488f2817b9ef91f939
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-14 14:14:00 -0500

    Added Search Bar as wel as search icon and home tab css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com?apikey=a030ee53";
 
 const App = () => {
-  return <h1> App </h1>;
+  //fetch movies
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL} &s= ${title}`);
+    const data = await response.json(); //wait for response but store info in "data"
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Shrek");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for Movies"
+          value="Superman" //static set value within movie serach bar
+          onChange={() => {}}
+        />
+
+        <img src={SearchIcon} alt="search" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
